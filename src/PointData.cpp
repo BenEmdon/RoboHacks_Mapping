5d01dcf344fbc1d7959fccb12c8910d1444c617c
BenEmdon <benjaminemdon@gmail.com>
2016-03-20 01:37:44 -0400

    Removing redundant head code

--- src/PointData.cpp
+++ src/PointData.cpp
@@ -7,16 +7,6 @@
 //
 
 #include "PointData.h"
-
-//
-//  PointData.cpp
-//  ROBO_mapping
-//
-//  Created by Benjamin Emdon on 2016-03-19.
-//  Copyright © 2016 Ben Emdon. All rights reserved.
-//
-
-//#include "PointData.h"
 #include "PointData.h"
 #include <assert.h>
 
